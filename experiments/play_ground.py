8049ebcca471de658c9b8d61684133b1daa4e8e3
Ching-Heng Lin <chingheng113@gmail.com>
2021-01-20 17:46:40 +0800

    bk

--- experiments/play_ground.py
+++ experiments/play_ground.py
@@ -1,22 +1,48 @@
 import pandas as pd
 import numpy as np
-import distython
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import distython
+import umap
+from tsnecuda import TSNE
 
+
+# tidy_data = pd.read_csv('vitalsing_data.csv')
+# tidy_data = tidy_data.iloc[4090:8189, :]
+# print(tidy_data.shape)
+# X_data = tidy_data.drop(['ID', 'CHT_NO', 'admin_date', 'discharge_date',
+#                          'AllMortality', 'CVDeath  ', 'Death Date', 'SurvivalWeeks'], axis=1)
+# y_data = tidy_data[['SurvivalWeeks']]
+# y_data = (y_data < 24).astype(int)
+# categorical_ix = [0, 2, 3, 4, 5, 6, 7, 8]
 
 tidy_data = pd.read_csv('tidy.csv')
+tidy_data = tidy_data.append(tidy_data, ignore_index=True)
+tidy_data = tidy_data.append(tidy_data, ignore_index=True)
+tidy_data = tidy_data.append(tidy_data, ignore_index=True)
+print(tidy_data.shape)
 X_data = tidy_data.drop(['CHT_No', 'reStroke'], axis=1)
 y_data = tidy_data[['reStroke']]
 categorical_ix = [0, 2, 3, 4, 5, 6, 7, 8, 34, 35, 36, 37, 38, 43, 44, 45,46, 48, 49, 50, 51, 52, 53, 55]
-categorical_columns = X_data.columns[categorical_ix].values
-numerical_columns =  np.setdiff1d(X_data.columns, categorical_columns)
 
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+categorical_columns = X_data.columns[categorical_ix].values
+numerical_columns = np.setdiff1d(X_data.columns, categorical_columns)
+
 
 X_data[numerical_columns] = StandardScaler().fit_transform(X_data[numerical_columns])
-df = pd.get_dummies(X_data, columns=categorical_columns)
-df_2 = pd.get_dummies(X_data, columns=categorical_columns, drop_first=True)
-df_2.to_csv()
-print('done')
+# one-hot
+X_data_one_hot = pd.get_dummies(X_data, columns=categorical_columns)
+
+heom_metric = distython.HEOM(X_data, categorical_ix,)
+
+tsne_embedding = TSNE(n_components=2, perplexity=15, learning_rate=10).fit_transform(X_data_one_hot)
+
+plt.scatter(
+    tsne_embedding[:, 0],
+    tsne_embedding[:, 1],
+    c=y_data.values.astype(int), s=1, cmap='Spectral')
+plt.gca().set_aspect('equal', 'datalim')
+
+print(X_data.head())
